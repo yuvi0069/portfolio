c51c0a866a00e878ca5f92f629d0133b820343f3
yuvi0069 <yuviabhi00@gmail.com>
2024-08-05 21:19:41 +0530

    first commit

--- pr2.js
+++ pr2.js
@@ -5,7 +5,7 @@ let name=document.getElementById("Name").value;
 let phone=document.getElementById("Pass").value;
 let email=document.getElementById("Email").value;
 let message=document.getElementById("Message").value;
-const a1=await fetch("http://127.0.0.1:3000/submit-form",{
+const a1=await fetch("/submit-form",{
     method: 'POST',
             headers: {
                 'Content-Type': 'application/json'
